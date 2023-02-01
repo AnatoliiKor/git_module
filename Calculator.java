fffd0d8fbef241c36c833705c0cfad2757f03ae8
Anatolii_Kornienko <anatolii_kornienko@epam.ua>
2023-02-01 17:40:26 +0200

    last commit

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+	public int add(int a, int b) {
+		System.out.println("This is add method");
+		return a + b;
+	}
+
+	public int add(int a, int b, int c) {
+                System.out.println("This is add method for 3 parameters");
+                return a + b + c;
+	}
+
+        public int add(int a, int b, int c, int d) {
+                System.out.println("This is add method for 4 parameters");
+                return a + b + c + d;
+        }
+
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a second part project for Git module
