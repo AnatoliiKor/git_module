8c8b31d398ddefc9489961a8118aaac6c74d5672
Anatolii_Kornienko <anatolii_kornienko@epam.ua>
2023-02-01 14:22:45 +0200

    Add method for 3 parameters

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,13 @@
 public class Calculator {
 
 	public int add(int a, int b) {
-		System.out.println("This is add method");
+		System.out.println("This is add method for 2 parameters");
 		return a + b;
 	}
+
+	public int add(int a, int b, int c) {
+                System.out.println("This is add method for 3 parameters");
+                return a + b + c;
+        }
+
 }
