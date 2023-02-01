982ed4f51ad296a9a70593b0ab649452b9344ece
Anatolii_Kornienko <anatolii_kornienko@epam.ua>
2023-02-01 12:08:13 +0200

    Squashing sqr, mult, div, sub commits to single math operations commit

--- Calculator.java
+++ Calculator.java
@@ -3,4 +3,22 @@ public class Calculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+
+	public int sub(int a, int b) {
+                return a - b;
+        }
+
+        public int div(int a, int b) {
+        	return a / b;
+        }
+
+	public int mult(int a, int b) {
+                return a * b;
+        }
+
+        public double sqrt(int a) {
+                return Math.sqrt(a);
+        }
+
+
 }
